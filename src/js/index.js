7a82caa36dca22ffbec8719c8975fd489379fed4
Roman Khalnepesov <khalnepesov@gmail.com>
2019-11-02 16:33:09 +0000

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,26 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suit = ["&#x2660", "&#x2663", "&#x2665", "&#x2666"];
+  let card = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
+
+  /* let cardGenerator = [];
+  suit.forEach(x => {
+    card.forEach(y => {
+      cardGenerator.push(x + " " + y);
+    });
+  });
+
+  let i = Math.floor(Math.random() * cardGenerator.length);
+
+
+
+  document.querySelector("#cards").innerHTML = cardGenerator[i];
+  */
+
+  let i = Math.floor(Math.random() * suit.length);
+  let j = Math.floor(Math.random() * card.length);
+  document.querySelector("#suits").innerHTML = suit[i];
+  document.querySelector("#suits1").innerHTML = suit[i];
+  document.querySelector("#cards").innerHTML = card[j];
 };
