c13fa96873d053e82e4f022c55ae26f5ffebb31e
Roman Khalnepesov <khalnepesov@gmail.com>
2019-11-04 12:43:24 +0000

    Add red color to suits

--- src/js/index.js
+++ src/js/index.js
@@ -29,4 +29,15 @@ window.onload = function() {
   document.querySelector("#suits").innerHTML = suit[i];
   document.querySelector("#suits1").innerHTML = suit[i];
   document.querySelector("#cards").innerHTML = card[j];
+  if (suit[i] == "&#x2665" || suit[i] == "&#x2666") {
+    document.querySelector("#suits").classList =
+      "card-title w-100 d-flex justify-content-start pl-2 pt-2 mb-0 text-danger";
+    document.querySelector("#suits1").classList =
+      "card-title w-100 d-flex pl-2 pb-2 mb-0 rotateimg180 text-danger";
+  } else {
+    document.querySelector("#suits").classList =
+      "card-title w-100 d-flex justify-content-start pl-2 pt-2 mb-0";
+    document.querySelector("#suits1").classList =
+      "card-title w-100 d-flex pl-2 pb-2 mb-0 rotateimg180";
+  }
 };
